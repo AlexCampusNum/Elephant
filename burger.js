769d81684e15867f4fff2a7f44f2d49576c32dbe
Alex Chamoux <dev.alex.chamoux@gmail.com>
2024-05-16 13:55:18 +0200

    Commit init

---
+++ burger.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
+
+const showHideMenu = () => {
+    
+    menu.classList.toggle("scale-x-0");
+
+};
+
+menuBtn.addEventListener("click", showHideMenu);
---
+++ tailwindconfig.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFBFC0",
+        secondary: "#BE8183",
+        title: "#9C6264",
+        buttonhover: "#FF5449",
+      },
+      fontFamily: {
+        rondeur: ['"Poetsen One"', 'sans-serif'],
+        classique: ['"Roboto"', 'sans-serif'],
+      },
+    },
+  },
+};
